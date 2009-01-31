02f576c13409ac3cc52243df19bdd7285eab9afd
Ben Bleything <ben@bleything.net>
2009-01-31 12:51:00 -0800

    initial commit, though there isn't much going on yet

---
+++ applette.rb
@@ -0,0 +1,63 @@
+#!/usr/bin/env ruby
+
+require 'rubygems'
+require 'compass'
+require 'sinatra'
+require 'sequel'
+
+
+########################################################################
+### H E L P E R S
+########################################################################
+helpers do
+  def partial( name )
+    # the haml method expects a symbol, so we have to do this stupid
+    # interning crap.
+    haml "partials/_#{name}".to_sym, :layout => false
+  end
+
+  def display_userbox
+    if session[ :user ]
+      # partial :userinfo
+    else
+      partial :login_note
+    end
+  end
+end
+
+########################################################################
+### S E T U P   A N D   F I L T E R S
+########################################################################
+configure do
+end
+
+before do
+  @page_title = "small ideas for small apps"
+  @header     = partial( :header )
+end
+
+########################################################################
+### A S S E T S
+########################################################################
+get '/styles.css' do
+  content_type 'text/css'
+
+  # Use views/stylesheets & blueprint's stylesheet dirs in the Sass
+  # loadpath
+  sass "css/styles".to_sym, {
+    :sass => {
+      :load_paths => (
+        [ File.join( File.dirname(__FILE__), 'views', 'css' ) ] +
+        Compass::Frameworks::ALL.map {|f| f.stylesheets_directory }
+      )
+    }
+  }
+end
+
+########################################################################
+### A C T I O N S
+########################################################################
+get '/' do
+  @header = ""
+  haml :index
+end
